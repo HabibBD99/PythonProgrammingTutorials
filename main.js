35c009fa29f1cbfa4f0e801c7fb3de6099bfe7ca
Habib <70913270+HabibBD99@users.noreply.github.com>
2022-10-16 21:56:44 +0600

    Create main.js
    js file added

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+
+/*
+    var Black = document.querySelector(".Card_Black");
+    Black.style.height="400px";
+    Black.style.width="80%";
+    Black.style.backgroundColor="black";
+    Black.style.marginLeft="10%";
+    Black.style.color="#fff";
+    Black.style.fontFamily="verdana,arial,roboto";
+*/
+
+
+
